587dff238cdb658033cb4b6a98bcaf36b65751b1
tyler smith <tsmith@dacima.test>
2024-03-05 17:26:19 -0500

    swtiching to GEE

---
+++ 03A_MixFol_PersonPeriod.R
@@ -0,0 +1,60 @@
+################################################################################
+# Environmental Mixtures and Plasma Folate in Pregnancy
+# Generalized Estimating Equations
+
+##### Preliminaries ############################################################
+# Load Packages
+library(tidyverse)
+library(geepack)
+library(broom)
+
+##### Read Data ################################################################
+df = readRDS("E:/BBK22/1. BBK22_folates_Oct5 2023/Analysis/MixFol_Complete.rds")
+
+##### Select Data ##############################################################
+df_period = df %>%
+  select(
+    SUBJECT_ID,
+    FOL_TM1_TOTAL_LN,
+    FOL_TM3_TOTAL_LN,
+    contains("NO2"),
+    contains("O3"),
+    contains("PM25"),
+    contains("SO2"),
+    AGE,
+    EDUCATION,
+    RACE,
+    BIRTH_COUNTRY,
+    INCOME,
+    FOLIC_ACID = FOLIC_ACID_CAT,
+    HEALTHY_EATING,
+    SITE_ID
+  )
+
+##### Pivot Longer to Person-Period Dataset ####################################
+# Pivot Longer
+df_period = df_period %>%
+  pivot_longer(
+    cols = c(FOL_TM1_TOTAL_LN, FOL_TM3_TOTAL_LN), 
+    names_to = "VISIT", 
+    values_to = "FOL_TOTAL_LN"
+  )
+
+# Arrange Columns
+df_period = df_period %>%
+  select(SUBJECT_ID, VISIT, FOL_TOTAL_LN, everything())
+
+# Name Visits
+df_period = df_period %>%
+  mutate(
+    VISIT =
+      ifelse(VISIT == "FOL_TM1_TOTAL_LN", "1",
+      ifelse(VISIT == "FOL_TM3_TOTAL_LN", "3", NA))
+  )
+
+df_period %>% head()
+
+df_period = df_period %>%
+  arrange(SUBJECT_ID, VISIT)
+
+df_period %>% head()
